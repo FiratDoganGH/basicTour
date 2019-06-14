52771b9dc954e4af4ebf7a66fd9b25e75886ef8f
firatdogan <firatdogan@gmail.com>
2019-06-14 15:39:16 +0200

    Service Tests edited

--- src/test/java/com/mic/fleetmanagement/FleetmanagementApplicationTourServiceTests.java
+++ src/test/java/com/mic/fleetmanagement/FleetmanagementApplicationTourServiceTests.java
@@ -17,7 +17,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FleetmanagementApplicationTourServiceTests {
-
     @Autowired
     private TourService tourService;
 
